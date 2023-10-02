6d5214b26254878a6ec26d639e9c27ce0fa6876b
ROBERTO FIGUEREDO RODRIGUES <roberto.fr@unifesspa.local>
2023-10-02 17:44:53 -0300

    Voltando para a versão do Paulo

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 // Mais um console de log de teste:
 console.log("O Paulo fez esse commit sem você ver na aula")
-console.log("Enviando para o GitHub")
